f1b41f2ab37ed178d4ac673a6791fc26a2117c19
AVISHKA SHAVINDA <134716496+avishka33@users.noreply.github.com>
2024-02-05 08:59:49 -0800

    Avi win_lok

---
+++ Avi.bat
@@ -0,0 +1,81 @@
+  cls
+
+@ECHO OFF
+
+title Folder Locker
+
+if EXIST "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" goto UNLOCK
+
+if NOT EXIST Locker goto MDLOCKER
+
+:CONFIRM
+
+echo Are you sure you want to Lock the folder (Y/N)
+
+set/p "cho=>"
+
+if %cho%==Y goto LOCK
+
+if %cho%==y goto LOCK
+
+if %cho%==n goto END
+
+if %cho%==N goto END
+
+echo Invalid choice.
+
+timeout 1
+
+goto CONFIRM
+
+:LOCK
+
+ren Locker "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+
+attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+
+echo Folder locked
+
+timeout 3
+
+goto End
+
+:UNLOCK
+
+@echo off
+set "psCommand=powershell -Command "$pword = read-host 'Enter Password to Unlock folder' -AsSecureString ; ^
+    $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); ^
+        [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)""
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set password=%%p
+
+if NOT %password%== <මෙතනට පාස්වර්ඩ් එක ගහන්න/ enter password> goto FAIL
+
+attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+
+ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" Locker
+
+echo Folder Unlocked Successfully
+
+timeout 1
+
+goto End
+
+:FAIL
+
+echo Invalid Password
+
+timeout 1
+
+goto end
+
+:MDLOCKER
+
+md Locker
+
+echo Locker created successfully
+
+timeout 5
+
+goto End
+
+:End
